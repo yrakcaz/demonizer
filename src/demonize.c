3f4cc1f2ddea5398f8ba1a8127e89c1e8c3950da
Zackary Ayoun <ayoun_z@epita.fr>
2014-10-20 10:32:56 +0200

    Stop/Continue DONE!

--- src/demonize.c
+++ src/demonize.c
@@ -3,11 +3,13 @@
 static void display_help()
 {
     printf("Usage: dem [option] [command] ...\n");
-    printf("       --cmd, -c:\tif you have severals commands to demonize, use this option before each of them.\n");
-    printf("       --kill, -k:\tkill the process corresponding to the next index.\n");
-    printf("       --restart, -r:\trestart the process corresponding to the next index.\n");
-    printf("       --jobs, -j:\tdisplay jobs.\n");
-    printf("       --help, -h:\tdisplays this help.\n");
+    printf("       --cmd, -c:\t\tif you have severals commands to demonize, use this option before each of them.\n");
+    printf("       --kill, -k:\t\tkill the deamon corresponding to the next index.\n");
+    printf("       --restart, -r:\t\trestart the deamon corresponding to the next index.\n");
+    printf("       --stop, -s:\t\tstop the deamon corresponding to the next index.\n");
+    printf("       --continue, -cont:\tcontinue the running of the deamon corresponding to the next index if it was stopped.\n");
+    printf("       --jobs, -j:\t\tdisplay jobs.\n");
+    printf("       --help, -h:\t\tdisplays this help.\n");
 }
 
 static char **str_to_wordtab(char *str)
@@ -107,6 +109,40 @@ static s_list *get_args(int argc, char **argv)
                         kill(deamons[atoi(argv[i + 1])], SIGKILL);
                         execute(str_to_wordtab(process));
                     }
+                    else
+                        fprintf(stderr, "dem: this doesn't exist\n");
+                }
+                exit(0);
+            }
+            else if (!strcmp(argv[i], "-cont") || !strcmp(argv[i], "--continue"))
+            {
+                if (!(argv + i + 1) || !argv[i + 1])
+                {
+                    fprintf(stderr, "dem: use --help or -h option\n");
+                    exit(1);
+                }
+                else
+                {
+                    get_deamons();
+                    if (deamons[atoi(argv[i + 1])])
+                        kill(deamons[atoi(argv[i + 1])], SIGCONT);
+                    else
+                        fprintf(stderr, "dem: this doesn't exist\n");
+                }
+                exit(0);
+            }
+            else if (!strcmp(argv[i], "-s") || !strcmp(argv[i], "--stop"))
+            {
+                if (!(argv + i + 1) || !argv[i + 1])
+                {
+                    fprintf(stderr, "dem: use --help or -h option\n");
+                    exit(1);
+                }
+                else
+                {
+                    get_deamons();
+                    if (deamons[atoi(argv[i + 1])])
+                        kill(deamons[atoi(argv[i + 1])], SIGSTOP);
                     else
                         fprintf(stderr, "dem: this doesn't exist\n");
                 }
