b439897d19df089e3023106b6b229a45e5d3d94b
Zackary Ayoun <ayoun_z@epita.fr>
2014-10-28 23:14:29 +0100

    little fix

--- src/tools.c
+++ src/tools.c
@@ -12,22 +12,12 @@ s_list *add_to_list(int val, s_list *list)
 {
     s_list *node =  malloc(sizeof (s_list));
     node->val = val;
-    if (!list)
-        node->next = NULL;
-    else
-        node->next = list;
+    node->next = list;
     return node;
 }
 
 void destroy_list(s_list *list)
 {
-    if (!list)
-        return;
-    if (!(list->next))
-    {
-        free(list);
-        return;
-    }
     s_list *tmp = list;
     while (tmp)
     {
